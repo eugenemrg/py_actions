dbc9c34b0526c8f937ea3c854cd03061b614e0f3
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-28 00:57:38 +0300

    Added other test cases

--- test_hello.py
+++ test_hello.py
@@ -2,3 +2,15 @@ def test_greet():
     # test if the function is defined
     from hello import greet
     assert greet('Alice') == 'Hello, Alice'
+
+def test_greet_with_default():
+    from hello import greet
+    assert greet() == 'Hello, world!!!'
+
+def test_greet_with_empty_string():
+    from hello import greet
+    assert greet('') == 'Hello, '
+
+def test_greet_with_number():
+    from hello import greet
+    assert greet(123) == 'Hello, 123'
